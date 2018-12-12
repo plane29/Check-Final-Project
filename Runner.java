3a1609dfdebdeba890d663723cc33251aec57661
plane29 <44649458+plane29@users.noreply.github.com>
2018-12-12 01:02:54 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,12 +1,12 @@
 
 
-public class Runner implements Runnable{
+public class Runner implements Runnable{  //a very simple class, runner basically is used so we can listen to the mouse and reprint the screen
 	
 	public static final int FPS = 60;
 
-	public void run(){
+	public void run(){  //loop to run
 		while(true){
-			Main.listenToMouse();
+			Main.listenToMouse();  //calls the method listen to mouse which is in main
 		}
 
 	}
